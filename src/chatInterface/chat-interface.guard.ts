750636bf22f6da8dab4e5d15586f531e35087fd4
Vitaly <vitalyryabchich@gmail.com>
2021-06-01 09:27:41 +0300

    chat interface guard

--- src/chatInterface/chat-interface.guard.ts
+++ src/chatInterface/chat-interface.guard.ts
@@ -1,13 +1,31 @@
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { Observable } from 'rxjs';
+import { Request } from 'express';
+import { JwtService } from '@nestjs/jwt';
+
+export interface cookieRequest extends Request {
+  headers: {
+    cookie: string;
+  };
+}
 
 @Injectable()
 export class ChatInterfaceGuard implements CanActivate {
+  constructor(private jwtService: JwtService) {}
+
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
-    const request = context.switchToHttp().getRequest();
-    console.log(request.headers.cookie);
+    const req: cookieRequest = context.switchToHttp().getRequest();
+    const token: string = getCookieValueByName(req.headers.cookie, 'token');
+
+    const verifiedData = this.jwtService.verify(token);
+    if (!verifiedData) return false;
     return true;
   }
+}
+
+function getCookieValueByName(cookie: string, name: string) {
+  const match = cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
+  return match ? match[2] : '';
 }
--- src/chatInterface/chat-interface.module.ts
+++ src/chatInterface/chat-interface.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
+import { JwtModule, JwtService } from '@nestjs/jwt';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserSchema } from 'src/auth/dto/user.schema';
+import { secretValue } from 'src/constants/jwt.constants';
 import { ChatInterfaceController } from './chat-interface.controller';
 import { ChatInterfaceService } from './chat-interface.service';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: 'user', schema: UserSchema }])],
+  imports: [
+    MongooseModule.forFeature([{ name: 'user', schema: UserSchema }]),
+    JwtModule.register({
+      secret: secretValue,
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
   providers: [ChatInterfaceService],
   controllers: [ChatInterfaceController],
 })
